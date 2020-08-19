83885a0af7c17fe2171aa450de7fd0c782ae5bcf
loc-koan <loc.koan@yahoo.com>
2020-08-19 17:18:49 -0500

    added event respondWith on service worker, updated names in manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -55,4 +55,13 @@ self.addEventListener("fetch", function(evt) {
     );
 
     return;
-}});
+  }
+  
+  evt.respondWith( /* from 18.13, missing in 18.14 */
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+});
